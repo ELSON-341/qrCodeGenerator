dff41be7ad3dbfad27f547f0c79bb553475fffff
ELSON-341 <elson.sn314@gmail.com>
2024-02-23 09:00:03 -0300

    end project

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
 // seleções
 const qrInput = document.querySelector('#text')
 const qrBtn = document.querySelector('button')
+const main = document.querySelector('main')
+const img = document.querySelector('#qr-code img')
 
 // funções
 function generatorQrCode() {
     const inputVelue = qrInput.value
 
     if(!inputVelue) {
-        window.alert('Por favor, ditite algo!')
+        window.alert('Por favor, digite algo!')
         return
     }
+
+    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputVelue}`
+
+
+    img.addEventListener('load', () => {
+        main.classList.add('active')
+    })
+
 }
 // elventos
 qrBtn.addEventListener('click', () => {
     generatorQrCode()
+})
+
+qrInput.addEventListener('keydown', (e) => {
+    if(e.code === 'Enter') generatorQrCode()
+})
+
+qrInput.addEventListener('keyup', () => {
+    if(!qrInput.value) {
+        main.classList.remove('active')
+    }
 })
